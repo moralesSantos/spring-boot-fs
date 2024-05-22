5a07e8d34c1cb29a500aa824d847b2b762a5e5ee
luis <jmsantos879@gmail.com>
2024-05-22 14:28:44 -0400

    testing ci

--- backend/src/main/java/com/luis/Main.java
+++ backend/src/main/java/com/luis/Main.java
@@ -15,9 +15,8 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+
     }
-
-
 
     @Bean
     CommandLineRunner runner(CustomerRepository customerRepository){
