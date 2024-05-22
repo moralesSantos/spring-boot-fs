ee8be73e56026bfb0272e3bdb9dd699a32916863
luis <jmsantos879@gmail.com>
2024-05-22 01:27:28 -0400

    added new line and removed unused import

--- backend/src/main/java/com/luis/Main.java
+++ backend/src/main/java/com/luis/Main.java
@@ -8,8 +8,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 import java.util.Random;
 
 
@@ -17,6 +15,8 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+
+
     }
 
     @Bean
