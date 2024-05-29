46031356a18b04c6622532f848c9c34b3e9b29b5
luis <jmsantos879@gmail.com>
2024-05-29 13:25:43 -0400

    break exitsPersonWithEmail method

--- backend/src/main/java/com/luis/customer/CustomerJDBCDataAccessService.java
+++ backend/src/main/java/com/luis/customer/CustomerJDBCDataAccessService.java
@@ -59,7 +59,7 @@ public class CustomerJDBCDataAccessService implements CustomerDAO {
         var sql = """
                 SELECT count(id)
                 FROM customer 
-                WHERE email = ?
+                WHERE name = ?
                 """;
         Integer count = jdbcTemplate.queryForObject(sql, Integer.class, email);
         return count != null && count > 0;
